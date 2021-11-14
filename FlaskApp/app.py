347062c8e5236148ec2df3487f8b205cc1636fc6
ReShadowww <justas.adomas.l@gmail.com>
2021-11-14 20:39:36 +0200

    first commit

---
+++ FlaskApp/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for, request, jsonify
+import json
+import random
+
+app = Flask(__name__)
+
+with open("lt-alias-picked.json", "r", encoding="UTF-8") as fin:
+    words = json.load(fin)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/_new_card')
+def add_numbers():
+    return jsonify(gen_cards())
+
+
+def gen_cards():
+    cards = []
+    i = 1
+    while i < 9:
+        rand = random.randint(0, len(words) - 1)
+        if words[rand]["kalbos_dalis"] and int(words[rand]["pavartojimo_daznis"]) > 1:
+            cards.append(words[rand]["zodis"].capitalize())
+            i += 1
+    return cards
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005)
+    # app.run(debug=True)
